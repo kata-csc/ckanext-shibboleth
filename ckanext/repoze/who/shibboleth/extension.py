dd1bdc1c89fbd7ac84e1d56bfe9208d0bc45c51f
Harri Palojärvi <harri.palojarvi@nomovok.com>
2012-09-05 09:13:16 +0200

    Small fixes, changes and more tests.

--- ckanext/repoze/who/shibboleth/extension.py
+++ ckanext/repoze/who/shibboleth/extension.py
@@ -8,14 +8,21 @@ from pylons import request
 import logging
 log = logging.getLogger("ckanext.repoze")
 
+def shib_urls():
+    return [url_for(controller='user', action='login'),
+            url_for(controller='user', action='register'),
+            url_for(controller='user', action='logged_out_page')]
+
+
 class CkanShibbolethPlugin(SingletonPlugin):
     implements(IRoutes, inherit=True)
     implements(IGenshiStreamFilter)
 
     def filter(self, stream):
         routes = request.environ.get('pylons.routes_dict')
+        from ckan.lib.base import request as req
         
-        if routes.get('controller') == 'user' and routes.get('action') == 'login':
+        if req.path in shib_urls():
             shtml = '<li class=""><a href="%s">Shibboleth login</a></li>' % url_for(controller='ckanext.repoze.who.shibboleth.controller:ShibbolethController', action='shiblogin')
             
             stream = stream | Transformer('//ul[@class="nav nav-pills"]').append(HTML(shtml))
@@ -24,9 +31,8 @@ class CkanShibbolethPlugin(SingletonPlugin):
     
     def before_map(self, map):
         controller = 'ckanext.repoze.who.shibboleth.controller:ShibbolethController'
-        map.connect('shibboleth', '/user/shiblogin', controller=controller, action='shiblogin')
+        map.connect('shibboleth', '/shibboleth/login', controller=controller, action='shiblogin')
         return map
     
     def after_map(self, map):
-        log.debug(map)
         return map
