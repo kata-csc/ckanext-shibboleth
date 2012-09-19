187fb9857d427e43bd0c21c5d71d1a89849a8150
Aleksi Suomalainen <aleksi.suomalainen@nomovok.com>
2012-09-19 16:03:02 +0300

    Add shibboleth login to new page layout.

--- ckanext/repoze/who/shibboleth/extension.py
+++ ckanext/repoze/who/shibboleth/extension.py
@@ -1,9 +1,10 @@
 from ckan.plugins import implements, SingletonPlugin
-from ckan.plugins import IGenshiStreamFilter, IRoutes
+from ckan.plugins import IGenshiStreamFilter, IRoutes, IConfigurer
 from genshi.filters.transform import Transformer
 from genshi.input import HTML
 from routes import url_for
 from pylons import request
+import os
 
 import logging
 log = logging.getLogger("ckanext.repoze")
@@ -11,6 +12,7 @@ log = logging.getLogger("ckanext.repoze")
 class CkanShibbolethPlugin(SingletonPlugin):
     implements(IRoutes, inherit=True)
     implements(IGenshiStreamFilter)
+    implements(IConfigurer)
 
     def filter(self, stream):
         routes = request.environ.get('pylons.routes_dict')
@@ -21,7 +23,23 @@ class CkanShibbolethPlugin(SingletonPlugin):
             stream = stream | Transformer('//ul[@class="nav nav-pills"]').append(HTML(shtml))
         
         return stream
-    
+
+    def update_config(self, config):
+        """This IConfigurer implementation causes CKAN to look in the
+        ```public``` and ```templates``` directories present in this
+        package for any customisations.
+
+        It also shows how to set the site title here (rather than in
+        the main site .ini file), and causes CKAN to use the
+        customised package form defined in ``package_form.py`` in this
+        directory.
+        """
+        here = os.path.dirname(__file__)
+        rootdir = os.path.dirname(os.path.dirname(here))
+        template_dir = os.path.join(rootdir, 'who', 'shibboleth', 'templates')
+        config['extra_template_paths'] = ','.join([template_dir,
+                config.get('extra_template_paths', '')])
+
     def before_map(self, map):
         controller = 'ckanext.repoze.who.shibboleth.controller:ShibbolethController'
         map.connect('shibboleth', '/user/shiblogin', controller=controller, action='shiblogin')
